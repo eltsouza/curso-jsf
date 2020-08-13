2c18341e89d7c8f67b8752a60167bae4e9eb070d
Alex Fernando Egidio <Alex Fernando Egidio@LAPTOP-VJTFURHL>
2020-08-13 15:04:53 -0300

    wewewe

--- src/main/java/br/com/jpautil/JPAUtil.java
+++ src/main/java/br/com/jpautil/JPAUtil.java
@@ -1,5 +1,6 @@
 package br.com.jpautil;
 
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
@@ -8,24 +9,24 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 @ApplicationScoped
-public class JPAUtil{
+public class JPAUtil {
 
 	private EntityManagerFactory factory = null;
-	
-	private JPAUtil() {
-      if (factory == null) {
-  		factory = Persistence.createEntityManagerFactory("curso-jsf"); // nome do persistence unit no arquivo persistence.xml
-      }
+
+	public JPAUtil() {
+		if (factory == null) {
+			factory = Persistence
+					.createEntityManagerFactory("curso-jsf");
+		}
 	}
-	
+
 	@Produces
 	@RequestScoped
 	public EntityManager getEntityManager() {
 		return factory.createEntityManager();
 	}
-	
+
 	public Object getPrimayKey(Object entity) {
-		
 		return factory.getPersistenceUnitUtil().getIdentifier(entity);
 	}
 
